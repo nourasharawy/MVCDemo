e0ff81c9282e86b16f274b6dfed252805852c358
nourasharawy <nourasharawy056@gmail.com>
2023-05-18 17:54:25 +0300

    Add files via upload

---
+++ MVCDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCDemo.Models;
+namespace MVCDemo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public ActionResult Index(int DepartmentId) 
+        {
+            EmployeeContext employeeContext = new EmployeeContext();
+           List <Employee> employees = employeeContext.Employees.Where(emp=> emp.DepartmentId == DepartmentId).ToList();
+            return View(employees); 
+        }
+
+        // GET: Employee
+        public ActionResult Details(int id)
+        {
+            EmployeeContext employeeContext = new EmployeeContext();
+            Employee emp = employeeContext.Employees.Single(e => e.EmployeeId == id); 
+            return View(emp);
+        }
+    }
+}
